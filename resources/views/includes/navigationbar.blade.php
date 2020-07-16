66797bce71786a2810a0204bd7b4f70e656c4bb3
tao-pipat <tao.pipat1995@gmail.com>
2020-07-16 11:56:43 +0700

    update link logo

--- resources/views/includes/navigationbar.blade.php
+++ resources/views/includes/navigationbar.blade.php
@@ -1,6 +1,6 @@
 <div class="app-header header-shadow">
     <div class="app-header__logo">
-        <div class="logo-src"></div>
+        <a href="{{ url('/') }}"><div class="logo-src"></div></a>
         <div class="header__pane ml-auto">
             <div>
                 <button type="button" class="hamburger close-sidebar-btn hamburger--elastic"
