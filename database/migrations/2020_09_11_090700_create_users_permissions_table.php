b2884dcb03396db0c70284017931ba8faed491fd
tao-pipat <tao.pipat1995@gmail.com>
2020-09-11 09:33:46 +0700

    create model permissions & migration

--- database/migrations/2020_09_11_090700_create_users_permissions_table.php
+++ database/migrations/2020_09_11_090700_create_users_permissions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class RoleUser extends Migration
+class CreateUsersPermissionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,16 @@ class RoleUser extends Migration
      */
     public function up()
     {
-        Schema::create('roles_user', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('roles_id')->unsigned();
-            $table->integer('user_id')->unsigned();
-            $table->timestamps();
+        Schema::create('users_permissions', function (Blueprint $table) {
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('permission_id');
 
             //FOREIGN KEY CONSTRAINTS
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('roles_id')->references('id')->on('roles')->onDelete('cascade');
-
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+ 
             //SETTING THE PRIMARY KEYS
-            $table->primary(['user_id', 'roles_id']);
+            $table->primary(['user_id','permission_id']);
         });
     }
 
@@ -35,6 +33,6 @@ class RoleUser extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('roles_user');
+        Schema::dropIfExists('users_permissions');
     }
 }
