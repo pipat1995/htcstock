017b91717a4da291590cfb2a4732f80f1b8c3191
pipat1995 <tao.pipat1995@gmail.com>
2020-12-24 16:43:41 +0700

    add btn edit image accessories

--- resources/views/it/all/index.blade.php
+++ resources/views/it/all/index.blade.php
@@ -9,14 +9,14 @@
             @isset($accessories)
             @foreach ($accessories as $item)
             <div class="col-md-4">
-                <div class="main-card mb-3 card"><img width="100%"
-                        src="{{url('storage/'.$item->image)}}" alt="Card image cap"
-                        class="card-img-top">
+                <div class="main-card mb-3 card"><img width="100%" src="{{url('storage/'.$item->image)}}"
+                        alt="Card image cap" class="card-img-top">
                     <div class="card-body">
                         <h5 class="card-title">{{$item->access_name}}</h5>
                         <h6 class="card-subtitle">{{$item->unit}}</h6>Some quick example text to build on the card title
                         and make up the bulk of the card's content.
-                        <button class="btn btn-secondary">Button</button>
+                        <a href="{{route('it.equipment.management.edit',$item->access_id)}}"><button type="button"
+                                class="btn btn-secondary btn-sm float-center mr-1">{{ __('itstock.manage-accessorie.detail') }}</button></a>
                     </div>
                 </div>
             </div>
